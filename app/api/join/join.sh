02ee4d4c5c6a3363bbf8d15f42892c26bcb325e9
emeterrservers <140479481+emeterrservers@users.noreply.github.com>
2023-07-31 10:33:25 +0100

    Update join.sh

--- app/api/join/join.sh
+++ app/api/join/join.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 API_KEY="verlink_default_secret"
-MIROTALK_URL="https://verlink2.onrender.com/api/v1/join"
+MIROTALK_URL="https://sfu.mirotalk.com/api/v1/join"
 # MIROTALK_URL="http://localhost:3010/api/v1/join"
 
 curl $MIROTALK_URL \
