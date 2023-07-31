492e5880eca41fdc064bf36118370ee830426403
emeterrservers <140479481+emeterrservers@users.noreply.github.com>
2023-07-31 10:41:04 +0100

    Update join.py

--- app/api/join/join.py
+++ app/api/join/join.py
@@ -2,7 +2,7 @@
 import requests
 import json
 
-API_KEY = "verlink_default_secret"
+API_KEY = "mirotalksfu_default_secret"
 MIROTALK_URL = "https://sfu.mirotalk.com/api/v1/join"
 # MIROTALK_URL = "http://localhost:3010/api/v1/join"
 
