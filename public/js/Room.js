28b17529baee2b2608d4c98e983aa5f014da8b71
emeterrservers <140479481+emeterrservers@users.noreply.github.com>
2023-07-31 10:49:35 +0100

    Update Room.js

--- public/js/Room.js
+++ public/js/Room.js
@@ -102,7 +102,6 @@ let joinRoomWithScreen = false;
 let initAudioButton = null;
 let initVideoButton = null;
 let initAudioVideoButton = null;
-let initSpeakerSelect = null
 let recTimer = null;
 let recElapsedTime = null;
 
